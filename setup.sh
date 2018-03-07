879eaf013ece95f77e176c2c3d0b2ff1ae50ea0a
HimanshuK18 <himanshukhurana18@gmail.com>
2018-03-07 18:46:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
 sudo apt-get update
 sudo apt-get install -y build-essential libssl-dev git curl
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
-
-wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
-tar -xf go1.7.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.9.4.linux-amd64.tar.gz
+sudo tar -xvf go1.9.4.linux-amd64.tar.gz
 sudo cp -r go/ /usr/local/
-rm -rf go/ go1.7.linux-amd64.tar.gz
+rm -rf go/ go1.9.4.linux-amd64.tar.gz
 echo "export GOROOT=/usr/local/go" >> ~/.bashrc
 echo "export GOPATH=$HOME/projects/go" >> ~/.bashrc
 echo "PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
